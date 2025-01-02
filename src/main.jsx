e925e1139d0c6896972fc9f89e82171d059da3e2
jelly3105 <anjalisherikar3105@gmail.com>
2025-01-02 12:07:39 +0530

    feat: added new version files
    BREAKING CHANGE: Create new version

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import Version1 from './versions/version1';
 import './index.css';
-// import Version2 from './versions/version2';
+import Version2 from './versions/version2';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Version1 />
-    {/* <Version2 /> */}
+    <Version2 />
   </React.StrictMode>
 );
---
+++ src/versions/version2.jsx
@@ -0,0 +1,11 @@
+function Version2() {
+    return (
+      <>
+        <header>
+          <h1>Version2 Added</h1>
+        </header>
+      </>
+    );
+}
+
+export default Version2;
