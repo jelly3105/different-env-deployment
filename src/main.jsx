5aaafffafbfda3dd8c19cf5b3751548e3a1abaaa
jelly3105 <anjalisherikar3105@gmail.com>
2025-01-02 13:43:40 +0530

    Revert "feat: version 3"
    This reverts commit e6c169269902a74e43025e8990976ed4a5eac5c5.

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,11 @@ import App from './App';
 import Version1 from './versions/version1';
 import './index.css';
 import Version2 from './versions/version2';
-import Version3 from './versions/version3';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Version1 />
     <Version2 />
-    <Version3 />
   </React.StrictMode>
 );
