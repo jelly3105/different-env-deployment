6b3b39dc32dc204a62e6a723913e14a62c13bfd2
jelly3105 <anjalisherikar3105@gmail.com>
2025-01-02 14:29:29 +0530

    feat: add version 4

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Version1 from './versions/version1';
 import './index.css';
 import Version2 from './versions/version2';
 import Version3 from './versions/version3';
+import Version4 from './versions/version4';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,5 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Version1 />
     <Version2 />
     <Version3 />
+    <Version4 />
   </React.StrictMode>
 );
---
+++ src/versions/version4.jsx
@@ -0,0 +1,11 @@
+function Version4() {
+    return (
+      <>
+        <header>
+          <h1>Version4 Added</h1>
+        </header>
+      </>
+    );
+}
+
+export default Version4;
